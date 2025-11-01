c6c76b95f381424aa30d46dd1250dc6068784e8b
Ivan Buruikov <la2espade@andex.com>
2025-11-01 23:36:15 +0300

    Initial Commit

---
+++ CryptoPortfolio_Tracker/CryptoPortfolio_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolio_TrackerApp.swift
+//  CryptoPortfolio_Tracker
+//
+//  Created by One Man Studio on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolio_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
